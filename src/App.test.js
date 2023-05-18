64b9c85b81020d76aaf13a050f3eabc7a923abb3
Siegfried <siegfried.bozza@renuo.ch>
2023-05-18 17:08:01 +0200

    update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders App div', () => {
-  const { container } = render(<App />);
-  const div = container.querySelector(".App")
-  expect(div).toBeInTheDocument();
-});
+describe('App component', () => {
+  let container;
+
+    beforeEach(() => {
+        container = render(<App />).container;
+    })
+
+    test('renders App div', () => {
+      const div = container.querySelector(".App")
+      expect(div).toBeInTheDocument();
+    });
+})
