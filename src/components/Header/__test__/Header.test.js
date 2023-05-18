444b3cd264d154df3cfc500dc26c9f9882d31cfe
Siegfried <siegfried.bozza@renuo.ch>
2023-05-18 18:38:40 +0200

    Delete  App test + Add Header test

---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react'
+import Header from '../Header'
+
+describe('Header component', () => {
+
+    // GET BY --- HAPPY PATH
+    it('renders the text passed in title prop', () => {
+        const title = "Todo"
+        render(<Header title={title} />)
+        const headingElement = screen.getByText(title)
+        expect(headingElement).toBeInTheDocument()
+    })
+
+    it('renders the text passed in title prop', () => {
+        const title = "Todo"
+        render(<Header title={title} />)
+        const headingElement = screen.getByRole('heading')
+        expect(headingElement).toBeInTheDocument()
+    })
+
+    it('renders only 1 heading', () => {
+        const title = "Todo"
+        render(<Header title={title} />)
+        const headingElements = screen.getAllByRole('heading')
+        expect(headingElements.length).toBe(1)
+    })
+
+    // FIND BY --- HAPPY PATH
+    it('renders the text passed in title prop', async () => {
+        const title = "Todo"
+        render(<Header title={title} />)
+        const headingElement = await screen.findByText(title)
+        expect(headingElement).toBeInTheDocument()
+    })
+
+    // QUERY BY --- UNHAPPY PATH
+    it('does not render a different text than the one passed in title prop', () => {
+        const title = "Todo"
+        render(<Header title={title} />)
+        const absentElement = screen.queryByText("random text")
+        expect(absentElement).not.toBeInTheDocument()
+    })
+})
